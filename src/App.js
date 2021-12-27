57915065ce73f1cd0cfe8788679a603117e7b62c
Nino Hasl <nino.hasl2@gmail.com>
2021-12-27 15:07:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { observer } from "mobx-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Sequencer from "./components/Sequencer/Sequencer/Sequencer";
+import Oscillator from "./components/Oscillator/Oscillator";
+import Table from "./components/Table/Table";
+
+import StateManager from "./StateManager";
+
+const stateManager = new StateManager();
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h3>start</h3>
+                {stateManager.toggleOscillator && (
+                    <Oscillator stateManager={stateManager} />
+                )}
+                <Sequencer stateManager={stateManager} />
+            </div>
+        );
+    }
 }
 
-export default App;
+export default observer(App);
